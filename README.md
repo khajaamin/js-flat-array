e7a67347fd3534fe618568574581a374e0a1cbc5
khajaamin <khajaamin@gmail.com>
2022-07-02 11:32:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flat-array-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-hkgcu9)
---
+++ index.js
@@ -0,0 +1,25 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const arr = [10, 20, 30, [10, 30, 50], [23, 32, [43, 67, 64]]];
+const toString = JSON.stringify(arr).replaceAll(/[\[\]]/g, '', true);
+console.log('--->', toString.split(',').map(Number));
+
+//
+function show(list) {
+  const newList = [];
+  list.map((item) => {
+    if (Array.isArray(item)) {
+      newList.push(...show(item));
+    } else {
+      newList.push(item);
+    }
+  });
+  return newList;
+}
+
+console.log(show(arr));
