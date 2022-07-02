bc227e96834f21a7db333a2eb6e60217236b9870
khajaamin <khajaamin@gmail.com>
2022-07-02 11:33:01 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS convert nested to flat array</h1>`;
 
 const arr = [10, 20, 30, [10, 30, 50], [23, 32, [43, 67, 64]]];
 const toString = JSON.stringify(arr).replaceAll(/[\[\]]/g, '', true);
